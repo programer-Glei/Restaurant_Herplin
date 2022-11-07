e571bd7de136128cf1cbf58e8125e39555b1d8ec
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-11-07 10:01:30 -0300

    proximo comit

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.11/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/estilo.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <section class="flex">
+            <a href="index.php" class="logo">Yum-yum</a>
+            <nav class="navbar">
+            <a href="index.php" class="logo">Home</a>
+            <a href="index.php" class="logo">Yum-yum</a>
+            </nav>
+        </section>
+    </header>
+</body>
+</html>
