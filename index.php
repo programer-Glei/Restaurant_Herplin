8209b22dc98f897d4bfb73936d3a6c67701620d8
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-11-07 10:29:12 -0300

    proximo comit

--- index.php
+++ index.php
@@ -30,6 +30,8 @@
             <div class="icons">
                 <a href="search.php"><i class="fas fa-search"></i></a>
                 <a href="cart.php"><i class="fas fa-shopping-cart"></i> <span>(3)</span> </a>
+                <div id="user-btn" class="fas fa-user"></div>
+                <div id="menu-btn" class="fas fa-bars"></div>
             </div>
         </section>
     </header>
