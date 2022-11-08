1af66c5e4a10e63c05e210b548ed40f5cc3199e2
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-11-08 09:34:14 -0300

    proximo comit

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- font awesome cdn link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.11/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
     <!-- custom css file link -->
     <link rel="stylesheet" href="css/estilo.css">
