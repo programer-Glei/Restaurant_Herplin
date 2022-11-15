7300440a9e8adef8fc62f03847999ef829d164b0
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-11-15 20:04:15 -0300

    proximo comit

--- index.php
+++ index.php
@@ -117,6 +117,24 @@
             <form action="" method="post" class="box">
                 <button type="submit" class="fas fa-eye" name="quick_view"></button>
                 <button type="submit" class="fas fa-shopping-cart" name="add_to_cart"></button>
+                <img src="img/burger-1.png" alt="imagem de Hamburguer">
+                <a href="#">Comida rápida</a>
+                <div class="name">X tudo</div>
+                <div class="flex">
+                    <div class="price"> <span>R$</span>15 </div>
+                    <input type="number" name="qty" id="" class="qty" min="1" max="99" value="1" onkeypress="if(this.value.length == 2) return false;">
+                </div>
+            </form>
+            <form action="" method="post" class="box">
+                <button type="submit" class="fas fa-eye" name="quick_view"></button>
+                <button type="submit" class="fas fa-shopping-cart" name="add_to_cart"></button>
+                <img src="img/burger-1.png" alt="imagem de Hamburguer">
+                <a href="#">Comida rápida</a>
+                <div class="name">X tudo</div>
+                <div class="flex">
+                    <div class="price"> <span>R$</span>15 </div>
+                    <input type="number" name="qty" id="" class="qty" min="1" max="99" value="1" onkeypress="if(this.value.length == 2) return false;">
+                </div>
             </form>
         </div>
     </section>
