34d31dd3209baee915cb619000f3fe2c0c595ed8
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-11-29 09:49:16 -0300

    proximo comit

--- cart.php
+++ cart.php
@@ -61,14 +61,16 @@
         <div class="box-container">
             <form action="" method="post" class="box">
                 <button type="submit" class="fas fa-eye" name="quick_view"></button>
-                <button type="submit" class="fas fa-shopping-cart" name="add_to_cart"></button>
-                <img src="img/burger-1.png" alt="imagem de Hamburguer">
+                <button type="submit" class="fas fa-times" name="delete" onclick="return confirm('delete this item?');"></button>
+                <img src="img/dish-1.png" alt="imagem de Hamburguer">
                 <a href="#" class="cat">Comida rápida</a>
                 <div class="name">X tudo</div>
                 <div class="flex">
                     <div class="price"> <span>R$</span>15 </div>
                     <input type="number" name="qty" id="" class="qty" min="1" max="99" value="1" onkeypress="if(this.value.length == 2) return false;">
+                    <button type="submit" class="fas fa-edit"></button>
                 </div>
+                <div class="sub-total">Subtotal: <span>R$15,00</span> </div>
             </form>
         </div>
     </section>
