5081d01528803f0daa4a0ad8792e5eb845709fc4
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-12-20 10:25:56 -0300

    proximo comit

--- components/user_header.php
+++ components/user_header.php
@@ -2,11 +2,47 @@
 
     if(isset($message)){
         foreach($message as $message){
-            echo '<div class="message">
-            <span></span>
-            <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+            echo '
+            <div class="message">
+                <span>'.$message.'</span>
+                <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
             </div>';
         }
     }
 
 ?>
+
+<header class="header">
+
+        <section class="flex">
+            <a href="index.php" class="logo">Yum-yum</a>
+
+            <nav class="navbar">
+                <a href="index.php" class="logo">Home</a>
+                <a href="about.php" class="logo">Sobre nós</a>
+                <a href="menu.php" class="logo">Menu</a>
+                <a href="orders.php" class="logo">Vendas</a>
+                <a href="contact.php" class="logo">Contato</a>
+            </nav>
+
+            <div class="icons">
+                <?php
+                    $count_user_cart_items = $conn->prepare();
+                ?>
+                <a href="search.php"><i class="fas fa-search"></i></a>
+                <a href="cart.php"><i class="fas fa-shopping-cart"></i> <span>(3)</span> </a>
+                <div id="user-btn" class="fas fa-user"></div>
+                <div id="menu-btn" class="fas fa-bars"></div>
+            </div>
+
+            <div class="profile">
+                <p class="name">Gleibinho</p>
+                <div class="flex">
+                    <a href="profile.php" class="btn">Perfil</a>
+                    <a href="#" class="delete-btn">Sair</a>
+                </div>
+                <p class="acconut"><a href="login.php">Conecte-se</a> ou <a href="register.php">Cadastre-se</a> </p>
+            </div>
+
+        </section>
+    </header>
