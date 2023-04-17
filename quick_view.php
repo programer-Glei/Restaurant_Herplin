74864575ecdd8629a372f06662fe30a0135830a9
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-04-17 12:00:56 -0300

    proximo comit

--- quick_view.php
+++ quick_view.php
@@ -19,9 +19,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>Visualização</title>
 </head>
 <body>
-    
+    <?php include 'components/user_header.php'; ?>
 </body>
 </html>
