38bf3ce3042b64813366764a2f2bb5c8c14d853d
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-04-17 11:58:28 -0300

    proximo comit

---
+++ quick_view.php
@@ -0,0 +1,27 @@
+<?php
+    include 'components/connect.php';
+
+    session_start();
+
+    if(isset($_SESSION['user_id'])){
+        $user_id = $_SESSION['user_id'];
+    }else{
+        $user_id = '';
+    }
+
+    include 'components/add_cart.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
