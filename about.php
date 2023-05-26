b5ae581e718f4e7e2f52f324f8c30bf3d6a82f6a
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-05-26 11:53:53 -0300

    proximo comit

--- about.php
+++ about.php
@@ -1,3 +1,17 @@
+<?php
+    header('Content-Type: text/html; charset=utf-8');
+    include 'components/connect.php';
+
+    session_start();
+
+    if(isset($_SESSION['user_id'])){
+        $user_id = $_SESSION['user_id'];
+    }else{
+        $user_id = '';
+        header('location:index.php');
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -16,37 +30,9 @@
     <title>Sobre nós</title>
 </head>
 <body>
-    <header class="header">
-
-        <section class="flex">
-            <a href="index.php" class="logo">Yum-yum</a>
-
-            <nav class="navbar">
-                <a href="index.php" class="logo">Home</a>
-                <a href="about.php" class="logo">Sobre nós</a>
-                <a href="menu.php" class="logo">Menu</a>
-                <a href="orders.php" class="logo">Vendas</a>
-                <a href="contact.php" class="logo">Contato</a>
-            </nav>
-
-            <div class="icons">
-                <a href="search.php"><i class="fas fa-search"></i></a>
-                <a href="cart.php"><i class="fas fa-shopping-cart"></i> <span>(3)</span> </a>
-                <div id="user-btn" class="fas fa-user"></div>
-                <div id="menu-btn" class="fas fa-bars"></div>
-            </div>
-
-            <div class="profile">
-                <p class="name">Gleibinho</p>
-                <div class="flex">
-                    <a href="profile.php" class="btn">Perfil</a>
-                    <a href="#" class="delete-btn">Sair</a>
-                </div>
-                <p class="acconut"><a href="login.php">Conecte-se</a> ou <a href="register.php">Cadastre-se</a> </p>
-            </div>
-
-        </section>
-    </header>
+    <!-- header section starts -->
+    <?php include 'components/user_header.php'; ?>
+    <!-- header section ends -->
 
     <!-- about section starts -->
     <div class="heading">
