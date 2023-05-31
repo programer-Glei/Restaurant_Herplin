1ea145b5e2d14281a60a7d9b46c69f2a752947c9
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-05-31 11:29:15 -0300

    proximo comit

--- cart.php
+++ cart.php
@@ -8,6 +8,13 @@
         $user_id = $_SESSION['user_id'];
     }else{
         $user_id = '';
+    };
+
+    if(isset($_POST['delete'])){
+        $cart_id = $_POST['cart_id'];
+        $delete_cart_item = $conn->prepare("DELETE FROM `cart` WHERE id = ?");
+        $delete_cart_item->execute([$cart_id]);
+        $message[] = 'Item do carrinho deletado!';
     }
 
 ?>
