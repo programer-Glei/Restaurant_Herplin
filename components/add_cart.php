ea765e03007cc500465423d59ffebaaf1a7a12b5
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-06-05 11:46:25 -0300

    proximo comit

--- components/add_cart.php
+++ components/add_cart.php
@@ -15,6 +15,17 @@
             $image = filter_var($image, FILTER_SANITIZE_STRING);
             $qty = $_POST['qty'];
             $qty = filter_var($qty, FILTER_SANITIZE_STRING);
+
+            $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
+            $check_cart_numbers->execute([$name, $user_id]);
+
+            if($check_cart_numbers->rowCount() > 0){
+                $message[] = 'já adicionado ao carrinho!';
+            }else{
+                $insert_cart = $conn->prepare("INSERT INTO `cart`(user_id, pid, name, price, quantity, image) VALUES(?,?,?,?,?,?)");
+                $insert_cart->execute([$user_id, $pid, $name, $price, $qty, $image]);
+                $message[] = 'Adicionado no carrinho!';
+            }
         }
     }
 ?>
