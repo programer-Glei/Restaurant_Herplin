d4d9b24aa13b3bbf3aa69e6ff17878c202f7ccbe
Gleibson Pereira <gleibson.santos05@gmail.com>
2023-06-15 12:00:45 -0300

    proximo comit

--- chekout.php
+++ chekout.php
@@ -1,3 +1,13 @@
+<?php
+    include 'components/connect.php';
+
+    session_start();
+
+    if(isset($_SESSION['user_id'])){
+        $user_id = $_SESSION['user_id'];
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
